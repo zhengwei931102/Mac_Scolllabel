89481c4f0f1d27f1f4ebf90a615b20ca886268d5
zhengwei931102 <admin@admindemac-mini.local>
2017-02-07 15:28:06 +0800

    Initial Commit

---
+++ 自动滚动的文字/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  自动滚动的文字
+//
+//  Created by admin on 17/2/7.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ 自动滚动的文字/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  自动滚动的文字
+//
+//  Created by admin on 17/2/7.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
